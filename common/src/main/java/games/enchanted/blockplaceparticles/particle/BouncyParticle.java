0d6046ef71ac7a5fdfcbd97cb272b8ff068dc5a0
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-09-18 21:26:40 +0100

    refactor and separate spark particles
    sperate bounce physics and stretchy rendering into separate classes

---
+++ common/src/main/java/games/enchanted/blockplaceparticles/particle/BouncyParticle.java
@@ -0,0 +1,55 @@
+package games.enchanted.blockplaceparticles.particle;
+
+import games.enchanted.blockplaceparticles.ParticleInteractionsLogging;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.List;
+
+public abstract class BouncyParticle extends TextureSheetParticle {
+    protected boolean physics_canBounce = true;
+    protected float physics_bounceDecay = 0.85f;
+    protected int ticksAlive = 0;
+
+    /**
+     * A single quad particle has simple bouncing physics.
+     * Set {@link #physics_bounceDecay} in your particle constructor to adjust how much the particle should bounce off of things
+     *
+     * @param level  level
+     * @param x      x pos
+     * @param y      y pos
+     * @param z      z pos
+     * @param xSpeed x velocity
+     * @param ySpeed y velocity
+     * @param zSpeed z velocity
+     */
+    protected BouncyParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+        super(level, x, y, z, xSpeed, ySpeed, zSpeed);
+    }
+
+    protected float getTotalVelocity() {
+        return (float) Math.abs(this.xd + this.yd + this.zd);
+    }
+
+    @Override
+    public void tick() {
+        ticksAlive++;
+
+        if(age > 0 && physics_canBounce && this.hasPhysics) {
+            double xVel = this.xd;
+            double yVel = this.yd;
+            double zVel = this.zd;
+            if (xVel * xVel + yVel * yVel + zVel * zVel < Mth.square(100)) {
+                Vec3 vec3 = Entity.collideBoundingBox(null, new Vec3(xVel, yVel, zVel), this.getBoundingBox(), this.level, List.of());
+                this.xd = vec3.x == 0.0 ? -this.xd * physics_bounceDecay : vec3.x;
+                this.yd = vec3.y == 0.0 ? -this.yd * physics_bounceDecay : vec3.y;
+                this.zd = vec3.z == 0.0 ? -this.zd * physics_bounceDecay : vec3.z;
+            }
+        }
+
+        super.tick();
+    }
+}
--- common/src/main/java/games/enchanted/blockplaceparticles/particle/spark/FlyingSpark.java
+++ common/src/main/java/games/enchanted/blockplaceparticles/particle/spark/FlyingSpark.java
@@ -1,45 +1,30 @@
 package games.enchanted.blockplaceparticles.particle.spark;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.Camera;
+import games.enchanted.blockplaceparticles.particle.StretchyBouncyQuadParticle;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
-import java.util.List;
-
-public class FlyingSpark extends TextureSheetParticle {
-    private final boolean spark_canBounce = true;
-    private final float spark_wallBounceDecay = 0.85f;
-
-    private double spark_xVelocityBeforeBounce;
-    private double spark_startingYVelocity;
-    private double spark_zVelocityBeforeBounce;
-    private int spark_floorBounces = 0;
-    private int spark_ticksAlive = 0;
-    private double prevPrevX;
-    private double prevPrevY;
-    private double prevPrevZ;
+public class FlyingSpark extends StretchyBouncyQuadParticle {
+    private boolean hasDecreasedLifespan = false;
 
     protected FlyingSpark(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, SpriteSet spriteSet) {
-        super(level, x, y, z);
+        super(level, x, y, z, xSpeed, ySpeed, zSpeed);
         this.setSprite(spriteSet.get(this.random.nextInt(12), 12));
         this.gravity = Mth.randomBetween(this.random, 0.8F, 0.9F);
         this.friction = 1.0F;
-        this.xd = spark_xVelocityBeforeBounce = (xSpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806;
-        this.yd = spark_startingYVelocity = (ySpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806;
-        this.zd = spark_zVelocityBeforeBounce = (zSpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806;
 
-        this.prevPrevX = this.xo;
-        this.prevPrevY = this.yo;
-        this.prevPrevZ = this.zo;
+        this.xd = (xSpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806 * (this.random.nextFloat() > 0.95 ? 2 : 1);
+        this.yd = (ySpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806 * (this.random.nextFloat() > 0.95 ? 2 : 1);
+        this.zd = (zSpeed / 2) + (Math.random() * 3.0 - 1.5) * 0.05000000074505806 * (this.random.nextFloat() > 0.95 ? 2 : 1);
+
+        this.physics_bounceDecay = 0.6f;
 
         this.rCol = this.gCol = 1;
         this.bCol = 0.9f;
@@ -51,12 +36,8 @@ public class FlyingSpark extends TextureSheetParticle {
         this.quadSize = particleSize;
     }
 
-    private float getTotalVelocity() {
-        return (float) Math.abs(this.xd + this.yd + this.zd);
-    }
-
-    private float getTimeMultiplier(float v) {
-        float d = Math.abs(v) * 500;
+    private float getColourDecayFactor(float totalVelocity) {
+        float d = Math.abs(totalVelocity) * 500;
         float ticksToStartDarkeningAt;
         if(d > 200) {
             ticksToStartDarkeningAt = 5;
@@ -72,117 +53,32 @@ public class FlyingSpark extends TextureSheetParticle {
             ticksToStartDarkeningAt = 130;
         }
 
-        return spark_ticksAlive <= ticksToStartDarkeningAt ? 1f : 0.85f;
+        return this.ticksAlive <= ticksToStartDarkeningAt ? 1f : 0.85f;
     }
 
     @Override
     public void tick() {
-        spark_ticksAlive++;
-        this.prevPrevX = this.xo;
-        this.prevPrevY = this.yo;
-        this.prevPrevZ = this.zo;
+        super.tick();
 
         if(age > 0) {
             float totalVelocity = getTotalVelocity();
 
-            double xVel = this.xd;
-            double yVel = this.yd;
-            double zVel = this.zd;
-            if (this.hasPhysics && xVel * xVel + yVel * yVel + zVel * zVel < Mth.square(100)) {
-                Vec3 vec3 = Entity.collideBoundingBox(null, new Vec3(xVel, yVel, zVel), this.getBoundingBox(), this.level, List.of());
-                if(spark_canBounce) {
-                    spark_xVelocityBeforeBounce *= -spark_wallBounceDecay;
-                    spark_zVelocityBeforeBounce *= -spark_wallBounceDecay;
-                    this.xd = vec3.x == 0.0 ? spark_xVelocityBeforeBounce : vec3.x;
-                    this.zd = vec3.z == 0.0 ? spark_zVelocityBeforeBounce : vec3.z;
-                }
-            }
-            if(this.onGround) {
-                spark_floorBounces += 1;
-                double verticalBounceDecay = (double) spark_floorBounces / 2;
-                this.yd = Math.abs(spark_startingYVelocity) * Math.abs(1 - verticalBounceDecay);
-            }
-            if(this.onGround && Math.abs(this.yd) < 0.005) {
+            if(!hasDecreasedLifespan && this.onGround && !this.isParticleMoving()) {
                 this.age = this.lifetime - Mth.randomBetweenInclusive(this.random, 6, 12);
+                hasDecreasedLifespan = true;
             }
-
             if(!(this.age >= this.lifetime - 14)) {
-                this.rCol *= this.rCol > 0.95 ? 0.985f * getTimeMultiplier(totalVelocity) : 1f;
-                this.gCol *= this.gCol > 0.8 ? 0.984f * getTimeMultiplier(totalVelocity) : 1f;
-                this.bCol *= this.bCol > 0.35 ? 0.98f * getTimeMultiplier(totalVelocity) : 1f;
+                float colourDecayFactor = getColourDecayFactor(totalVelocity);
+                this.rCol *= this.rCol > 0.95 ? 0.985f * colourDecayFactor : 1f;
+                this.gCol *= this.gCol > 0.8 ? 0.984f * colourDecayFactor : 1f;
+                this.bCol *= this.bCol > 0.35 ? 0.98f * colourDecayFactor : 1f;
             }
         }
-
-        super.tick();
     }
 
     @Override
     public int getLightColor(float f) {
         return 240;
-    }
-
-    @Override
-    public @NotNull FacingCameraMode getFacingCameraMode() {
-        if(this.prevPrevX == this.x && this.prevPrevY == this.y && this.prevPrevZ == this.z) {
-            return FacingCameraMode.LOOKAT_XYZ;
-        }
-        return (quaternionf, camera, d) -> quaternionf.set(0.0f, 0.0f, 0.0f, camera.rotation().w);
-    }
-
-    @Override
-    protected void renderRotatedQuad(@NotNull VertexConsumer consumer, @NotNull Camera camera, @NotNull Quaternionf quaternionf, float d) {
-        Vec3 cameraPosition = camera.getPosition();
-        float xPos = (float) ((Mth.lerp(d,  this.xo, this.x) - cameraPosition.x()) );
-        float yPos = (float) ((Mth.lerp(d,  this.yo, this.y) - cameraPosition.y()) );
-        float zPos = (float) ((Mth.lerp(d,  this.zo, this.z) - cameraPosition.z()) );
-        Vector3f pos = new Vector3f(xPos, yPos, zPos);
-        float prevXPos = (float) ((Mth.lerp(d,  this.prevPrevX, this.xo) - cameraPosition.x()) );
-        float prevYPos = (float) ((Mth.lerp(d,  this.prevPrevY, this.yo) - cameraPosition.y()) );
-        float prevZPos = (float) ((Mth.lerp(d,  this.prevPrevZ, this.zo) - cameraPosition.z()) );
-        Vector3f prevPos = new Vector3f(prevXPos, prevYPos, prevZPos);
-        this.renderSparkQuads(consumer, quaternionf, pos, prevPos, d);
-    }
-
-    private void renderSparkQuads(@NotNull VertexConsumer consumer, @NotNull Quaternionf quaternionf, Vector3f pos, Vector3f prevPos, float d) {
-        float quadSize = this.getQuadSize(d);
-        float u0 = this.getU0();
-        float u1 = this.getU1();
-        float v0 = this.getV0();
-        float v1 = this.getV1();
-        int lightColor = this.getLightColor(d);
-
-        Vector3f particleDir = new Vector3f(0f, 0f, 0f);
-        pos.sub(prevPos, particleDir);
-        particleDir.x = Math.abs(0f - (Math.clamp(particleDir.x, -0.015f, 0.015f) + 0.015f)) - 0.015f;
-        particleDir.y = Math.abs(0f - (Math.clamp(particleDir.y, -0.015f, 0.015f) + 0.015f)) - 0.015f;
-        particleDir.z = Math.abs(0f - (Math.clamp(particleDir.z, -0.015f, 0.015f) + 0.015f)) - 0.015f;
-
-        this.renderVertex(consumer, quaternionf, pos.x     - particleDir.x, pos.y     - particleDir.y, pos.z     - particleDir.z,  1.0f, -1.0f, quadSize, u1, v1, lightColor); // bottom left
-        this.renderVertex(consumer, quaternionf, prevPos.x - particleDir.x, prevPos.y - particleDir.y, prevPos.z - particleDir.z,  1.0f,  1.0f, quadSize, u1, v0, lightColor); // top right
-        this.renderVertex(consumer, quaternionf, prevPos.x + particleDir.x, prevPos.y + particleDir.y, prevPos.z + particleDir.z, -1.0f,  1.0f, quadSize, u0, v0, lightColor); // top left
-        this.renderVertex(consumer, quaternionf, pos.x     + particleDir.x, pos.y     + particleDir.y, pos.z     + particleDir.z, -1.0f, -1.0f, quadSize, u0, v1, lightColor); // bottom right
-
-        this.renderVertex(consumer, quaternionf, pos.x     + particleDir.x, pos.y     + particleDir.y, pos.z     + particleDir.z, -1.0f, -1.0f, quadSize, u0, v1, lightColor); // bottom right
-        this.renderVertex(consumer, quaternionf, prevPos.x + particleDir.x, prevPos.y + particleDir.y, prevPos.z + particleDir.z, -1.0f,  1.0f, quadSize, u0, v0, lightColor); // top left
-        this.renderVertex(consumer, quaternionf, prevPos.x - particleDir.x, prevPos.y - particleDir.y, prevPos.z - particleDir.z,  1.0f,  1.0f, quadSize, u1, v0, lightColor); // top right
-        this.renderVertex(consumer, quaternionf, pos.x     - particleDir.x, pos.y     - particleDir.y, pos.z     - particleDir.z,  1.0f, -1.0f, quadSize, u1, v1, lightColor); // bottom left
-
-        Quaternionf rotatedQuaternion = new Quaternionf(quaternionf);
-        rotatedQuaternion.rotateLocalY((float) Math.toRadians(90));
-        this.renderVertex(consumer, rotatedQuaternion, pos.x     - particleDir.x, pos.y     - particleDir.y, pos.z     - particleDir.z,  1.0f, -1.0f, quadSize, u1, v1, lightColor); // bottom left
-        this.renderVertex(consumer, rotatedQuaternion, prevPos.x - particleDir.x, prevPos.y - particleDir.y, prevPos.z - particleDir.z,  1.0f,  1.0f, quadSize, u1, v0, lightColor); // top right
-        this.renderVertex(consumer, rotatedQuaternion, prevPos.x + particleDir.x, prevPos.y + particleDir.y, prevPos.z + particleDir.z, -1.0f,  1.0f, quadSize, u0, v0, lightColor); // top left
-        this.renderVertex(consumer, rotatedQuaternion, pos.x     + particleDir.x, pos.y     + particleDir.y, pos.z     + particleDir.z, -1.0f, -1.0f, quadSize, u0, v1, lightColor); // bottom right
-
-        this.renderVertex(consumer, rotatedQuaternion, pos.x     + particleDir.x, pos.y     + particleDir.y, pos.z     + particleDir.z, -1.0f, -1.0f, quadSize, u0, v1, lightColor); // bottom right
-        this.renderVertex(consumer, rotatedQuaternion, prevPos.x + particleDir.x, prevPos.y + particleDir.y, prevPos.z + particleDir.z, -1.0f,  1.0f, quadSize, u0, v0, lightColor); // top left
-        this.renderVertex(consumer, rotatedQuaternion, prevPos.x - particleDir.x, prevPos.y - particleDir.y, prevPos.z - particleDir.z,  1.0f,  1.0f, quadSize, u1, v0, lightColor); // top right
-        this.renderVertex(consumer, rotatedQuaternion, pos.x     - particleDir.x, pos.y     - particleDir.y, pos.z     - particleDir.z,  1.0f, -1.0f, quadSize, u1, v1, lightColor); // bottom left
-    }
-
-    private void renderVertex(VertexConsumer consumer, Quaternionf quaternionf, float x, float y, float z, float width, float height, float sizeMultiplier, float u, float v, int lightColor) {
-        Vector3f vertexPos = new Vector3f(width, height, 0.0f).rotate(quaternionf).mul(sizeMultiplier).add(x, y, z);
-        consumer.addVertex(vertexPos.x(), vertexPos.y(), vertexPos.z()).setUv(u, v).setColor(this.rCol, this.gCol, this.bCol, this.alpha).setLight(lightColor);
     }
 
     @Override
