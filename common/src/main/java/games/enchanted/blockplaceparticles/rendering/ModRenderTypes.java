d8e01be3733b4585fb40091a7e8480949fb6465d
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-03-24 22:20:17 +0000

    start fixing ModRenderTypes, need to figure out why AT/AW isnt applying properly

--- common/src/main/java/games/enchanted/blockplaceparticles/rendering/ModRenderTypes.java
+++ common/src/main/java/games/enchanted/blockplaceparticles/rendering/ModRenderTypes.java
@@ -1,8 +1,12 @@
 package games.enchanted.blockplaceparticles.rendering;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.MeshData;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.Util;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
@@ -10,24 +14,27 @@ import net.minecraft.util.TriState;
 
 import java.util.function.Function;
 
-public class ModRenderTypes extends RenderType {
-    public ModRenderTypes(String name, VertexFormat format, VertexFormat.Mode mode, int bufferSize, boolean affectsCrumbling, boolean sortOnUpload, Runnable setupState, Runnable clearState) {
-        super(name, format, mode, bufferSize, affectsCrumbling, sortOnUpload, setupState, clearState);
+public abstract class ModRenderTypes extends RenderType {
+    public ModRenderTypes(String p_173178_, int p_173181_, boolean p_173182_, boolean p_173183_, Runnable p_173184_, Runnable p_173185_) {
+        super(p_173178_, p_173181_, p_173182_, p_173183_, p_173184_, p_173185_);
     }
 
     private static final Function<ResourceLocation, RenderType> TRANSLUCENT_PARTICLE_BACKFACE = Util.memoize(ModRenderTypes::createTranslucentParticleBackface);
 
     private static RenderType createTranslucentParticleBackface(ResourceLocation location) {
+//        RenderType.CompositeState renderState = RenderType.CompositeState.builder()
+//            .setShaderState(PARTICLE_SHADER)
+//            .setTextureState(new RenderStateShard.TextureStateShard(location, TriState.FALSE, false))
+//            .setCullState(RenderStateShard.NO_CULL)
+//            .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+//            .setOutputState(PARTICLES_TARGET)
+//            .setLightmapState(LIGHTMAP)
+//            .setWriteMaskState(COLOR_DEPTH_WRITE)
+//            .createCompositeState(false);
         RenderType.CompositeState renderState = RenderType.CompositeState.builder()
-            .setShaderState(PARTICLE_SHADER)
             .setTextureState(new RenderStateShard.TextureStateShard(location, TriState.FALSE, false))
-            .setCullState(RenderStateShard.NO_CULL)
-            .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
-            .setOutputState(PARTICLES_TARGET)
-            .setLightmapState(LIGHTMAP)
-            .setWriteMaskState(COLOR_DEPTH_WRITE)
-            .createCompositeState(false);
-        return create("translucent_particle_backface", DefaultVertexFormat.PARTICLE, VertexFormat.Mode.QUADS, 1536, false, false, renderState);
+            .setLightmapState(LIGHTMAP).createCompositeState(false);
+        return RenderType.create("translucent_particle_backface", 1536, false, false, RenderPipelines.TRANSLUCENT_PARTICLE, renderState);
     }
 
     public static RenderType translucentParticleBackface(ResourceLocation location) {
