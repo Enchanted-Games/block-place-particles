886583f3d373410cea43e68a2437435e7f669ac4
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-09-23 21:08:33 +0100

    make fireflies not get stuck on blocks

--- src/main/java/games/enchanted/eg_particle_interactions/common/mixin/particles/FireflyParticleMixin.java
+++ src/main/java/games/enchanted/eg_particle_interactions/common/mixin/particles/FireflyParticleMixin.java
@@ -2,20 +2,38 @@ package games.enchanted.eg_particle_interactions.common.mixin.particles;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import games.enchanted.eg_particle_interactions.common.duck.ParticleAccess;
 import games.enchanted.eg_particle_interactions.common.particle_override.BlockParticleOverrides;
 import games.enchanted.eg_particle_interactions.common.registry.BlockOrTagLocation;
 import games.enchanted.eg_particle_interactions.common.registry.RegistryHelpers;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.FireflyParticle;
+import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 @Mixin(FireflyParticle.class)
-public class FireflyParticleMixin {
+public abstract class FireflyParticleMixin extends TextureSheetParticle {
+    protected FireflyParticleMixin(ClientLevel level, double x, double y, double z) {
+        super(level, x, y, z);
+    }
+
+    @Inject(
+        at = @At("TAIL"),
+        method = "<init>"
+    )
+    private void block_place_particle$makeFirefliesNotGetStuckOnStuff(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, CallbackInfo ci) {
+        ((ParticleAccess) this).eg_particle_interactions$setBypassMovementCollisionCheck(true);
+    }
+
     @WrapOperation(
         at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z"),
         method = "tick"
