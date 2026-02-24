5c3c5236e0cffa1d6b40ec4eb5fafd9190016cbb
ioblackshaw <isaacoblackshaw@gmail.com>
2026-02-24 21:45:13 +0000

    clean up firefly particle mixin

--- src/main/java/games/enchanted/eg_particle_interactions/common/mixin/client/particles/FireflyParticleMixin.java
+++ src/main/java/games/enchanted/eg_particle_interactions/common/mixin/client/particles/FireflyParticleMixin.java
@@ -4,12 +4,11 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import games.enchanted.eg_particle_interactions.common.config.categories.GeneralOptions;
 import games.enchanted.eg_particle_interactions.common.duck.ParticleAccess;
-import games.enchanted.eg_particle_interactions.common.particle.overrides.BlockParticleOverrides;
-import games.enchanted.eg_particle_interactions.common.registry.BlockOrTagLocation;
-import games.enchanted.eg_particle_interactions.common.registry.RegistryHelpers;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.FireflyParticle;
+import net.minecraft.client.particle.SingleQuadParticle;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,45 +16,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//? if minecraft: <= 1.21.8 {
-/*import net.minecraft.client.particle.TextureSheetParticle;
- *///?} else {
-import net.minecraft.client.particle.SingleQuadParticle;
-//?}
-
-import java.util.List;
-
 @Mixin(FireflyParticle.class)
-public abstract class FireflyParticleMixin
-    //? if minecraft: <= 1.21.8 {
-    /*extends TextureSheetParticle
-     *///?} else {
-    extends SingleQuadParticle
-//?}
-{
-    //? if minecraft: <=1.21.8 {
-    /*protected FireflyParticleMixin(ClientLevel level, double x, double y, double z) {
-        super(level, x, y, z);
-    }
-    *///?} else {
+public abstract class FireflyParticleMixin extends SingleQuadParticle {
     protected FireflyParticleMixin(ClientLevel level, double x, double y, double z, TextureAtlasSprite sprite) {
         super(level, x, y, z, sprite);
     }
-    //?}
 
-    //? if minecraft: <=1.21.8 {
-    /*@Inject(
-        at = @At("TAIL"),
-        method = "<init>"
-    )
-    private void block_place_particle$makeFirefliesNotGetStuckOnStuff(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, CallbackInfo ci) {
-    *///?} else {
     @Inject(
         at = @At("TAIL"),
         method = "<init>"
     )
     private void block_place_particle$makeFirefliesNotGetStuckOnStuff(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, TextureAtlasSprite textureAtlasSprite, CallbackInfo ci) {
-    //?}
         if(!GeneralOptions.FIREFLY_FIXES.getValue()) return;
         ((ParticleAccess) this).eg_particle_interactions$setBypassMovementCollisionCheck(true);
     }
@@ -68,13 +39,7 @@ public abstract class FireflyParticleMixin
         if(!GeneralOptions.FIREFLY_FIXES.getValue()) {
             return original.call(state);
         }
-        // TODO: fix firefly override blocks
-//        List<BlockOrTagLocation> fireflyOverrideBlocks = BlockParticleOverrides.FIREFLY.getSupportedBlocksAndTags();
-//        List<BlockOrTagLocation> grassBladeOverrideBlocks = BlockParticleOverrides.GRASS_BLADE.getSupportedBlocksAndTags();
-//        if(fireflyOverrideBlocks == null || grassBladeOverrideBlocks == null) {
-//            return original.call(state);
-//        }
-        if(state.is(Blocks.FIREFLY_BUSH)) {
+        if(state.is(Blocks.FIREFLY_BUSH) || state.is(BlockTags.REPLACEABLE) || state.is(BlockTags.FLOWERS)) {
             return original.call(Blocks.AIR.defaultBlockState());
         }
         return original.call(state);
