8f0a0ce8122fc605b13ada2888493f4a3802253d
ioblackshaw <isaacoblackshaw@gmail.com>
2026-04-21 20:01:29 +0100

    implement fallback for emitter rules

--- src/main/java/games/enchanted/eg_particle_interactions/common/particle/emitter/rule/EmitterRule.java
+++ src/main/java/games/enchanted/eg_particle_interactions/common/particle/emitter/rule/EmitterRule.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import games.enchanted.eg_particle_interactions.common.particle.ParticleContext;
 import games.enchanted.eg_particle_interactions.common.particle.emitter.Emitter;
+import games.enchanted.eg_particle_interactions.common.particle.emitter.Emitters;
 import games.enchanted.eg_particle_interactions.common.particle.emitter.EmptyEmitter;
 import games.enchanted.eg_particle_interactions.common.particle.emitter.rule.types.EmitterRuleType;
 import games.enchanted.eg_particle_interactions.common.particle.emitter.rule.types.EmitterRuleTypes;
@@ -11,35 +12,36 @@ import games.enchanted.eg_particle_interactions.common.particle.emitter.rule.typ
 import java.util.ArrayList;
 import java.util.List;
 
-public record EmitterRule(List<EmitterRuleType> rules) {
-    public static final EmitterRule EMPTY = new EmitterRule(List.of());
+public record EmitterRule(List<EmitterRuleType> rules, Emitter fallbackEmitter) {
+    public static final EmitterRule EMPTY = new EmitterRule(List.of(), EmptyEmitter.INSTANCE);
 
     public Emitter getEmitter(ParticleContext context) {
-        return this.getEmitter(context, EmptyEmitter.INSTANCE);
-    }
-
-    public Emitter getEmitter(ParticleContext context, Emitter fallback) {
         for (EmitterRuleType rule : rules()) {
             if(!rule.matches(context)) continue;
             return rule.getEmitter();
         }
-        return fallback;
+        return this.fallbackEmitter();
     }
 
     public static EmitterRule combineFiles(List<File> files) {
         List<EmitterRuleType> rules = new ArrayList<>();
+        Emitter fallback = EmptyEmitter.INSTANCE;
 
         for (File file : files) {
             rules.addAll(file.rules());
+
+            if(file.fallbackEmitter() instanceof EmptyEmitter) continue;
+            fallback = file.fallbackEmitter();
         }
 
-        return new EmitterRule(List.copyOf(rules));
+        return new EmitterRule(List.copyOf(rules), fallback);
     }
 
-    public record File(List<EmitterRuleType> rules) {
+    public record File(List<EmitterRuleType> rules, Emitter fallbackEmitter) {
         public static final Codec<File> CODEC = RecordCodecBuilder.create(
             i -> i.group(
-                Codec.list(EmitterRuleTypes.CODEC).fieldOf("rules").forGetter(File::rules)
+                Codec.list(EmitterRuleTypes.CODEC).optionalFieldOf("rules", List.of()).forGetter(File::rules),
+                Emitters.CODEC.optionalFieldOf("fallback_emitter",EmptyEmitter.INSTANCE).forGetter(File::fallbackEmitter)
             ).apply(
                 i,
                 File::new
