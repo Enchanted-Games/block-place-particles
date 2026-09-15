a0989e885f3f5eb2c2aae4b80ab16aca24d59e60
ioblackshaw <isaacoblackshaw@gmail.com>
2026-09-15 19:16:14 +0100

    disable 26.1 temporarily

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,7 @@ stonecutter {
 
         // use fabric_remap as the loader for obfuscated minecraft versions (1.21.11 or below)
 
-        ver("26.1", "fabric", "neoforge")
+//        ver("26.1", "fabric", "neoforge")
         ver("26.3", "fabric")
 
         vcsVersion = "26.3-fabric"
